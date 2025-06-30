db0c765b4400a5615b00cdca07dc6ff3f07fccd1
EmanReda1 <Emansaad603@gmail.com>
2025-06-30 22:39:30 +0300

    Fix JWT middleware alias in bootstrap/app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -5,6 +5,7 @@ use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
 
+
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__ . '/../routes/web.php',
@@ -15,8 +16,8 @@ return Application::configure(basePath: dirname(__DIR__))
 
       ->withMiddleware(function (Middleware $middleware) {
         $middleware->alias([
-            // ... أي aliases موجودة مسبقاً هنا (إذا وجدت) ...
-            'jwt.auth' => CheckForToken::class, // قم بتغيير هذا السطر
+
+           'jwt.auth' => Tymon\JWTAuth\Http\Middleware\Authenticate::class,
         ]);
     })
 
